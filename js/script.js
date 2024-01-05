6249b298c4dd07a04833415462666d67806e90dd
Dhruv Patel <dhruvpatel@d-h-r-u-v-s-mbp.home>
2024-01-04 22:48:41 -0500

    nav links are working with proper active links using JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
+/* ======================== Typing animation ============================= */
 var typed = new Typed(".typing", {
-    strings:["Web Developer", "Front-End Developer"],
+    strings:["", "Web Developer", "Front-End Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
 })
+
+/* ======================== Aside ============================= */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSelection = document.querySelectorAll(".section"),
+    totalSelection = allSelection.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for (let j = 0; j < totalNavList; j++)
+            {
+                navList[j].querySelector("a").classList.remove("active");
+                }
+            this.classList.add("active")
+            showSection(this);
+        })
+    }
+    function showSection(element)
+    {
+        for (let i = 0; i < totalSelection; i++)
+        {
+            allSelection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+    }
